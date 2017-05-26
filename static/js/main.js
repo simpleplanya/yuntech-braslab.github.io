32555c8670af4864d43e63e0017c748632c50c40
m85091081 <m85091081@gmail.com>
2017-05-26 15:10:31 +0800

    fix formated error

--- static/js/main.js
+++ static/js/main.js
@@ -1,24 +1,20 @@
-Vue.component('component-anno-list',{
-	data: function() {
-		return {
-			annolist:[]
-		}
+new Vue({
+	el: '#app',
+	data: {
+		annolist:[]
 	},
-	mounted: function(){
+	mounted: function ()	{
+	var self = this;
 	var xmlhttp = new XMLHttpRequest();
 	var url = "xmlhttp/annolist.txt";
 	xmlhttp.onreadystatechange = function() {
 	    if (this.readyState == 4 && this.status == 200) {
-				this.annolist = this.response.body; 
-		    }
+		self.annolist = self.responseText;
+		console.log(self.annolist)
+		}
 	};
 	xmlhttp.open("GET", url, true);
 	xmlhttp.send();
 	}
 
-
-})
-
-new Vue({
-	el: '#app',
 })
