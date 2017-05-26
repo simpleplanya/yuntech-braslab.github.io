f32ca9c9ed51b1ad72f93c53278ece96c76c1b27
m85091081 <m85091081@gmail.com>
2017-05-26 14:41:42 +0800

    text add annolist

---
+++ static/js/main.js
@@ -0,0 +1,24 @@
+Vue.component('component-anno-list',{
+	data: function() {
+		return {
+			annolist:[]
+		}
+	},
+	mounted: function(){
+	var xmlhttp = new XMLHttpRequest();
+	var url = "xmlhttp/annolist.txt";
+	xmlhttp.onreadystatechange = function() {
+	    if (this.readyState == 4 && this.status == 200) {
+				this.annolist = response.body; 
+		    }
+	};
+	xmlhttp.open("GET", url, true);
+	xmlhttp.send();
+	}
+
+
+})
+
+new Vue({
+	el: '#app',
+})
