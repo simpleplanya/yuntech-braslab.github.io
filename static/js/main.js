2dacda7492f70aee745985ae5dd0ad89377e05e3
m85091081 <m85091081@gmail.com>
2017-05-26 15:47:43 +0800

    fix js and style

--- static/js/main.js
+++ static/js/main.js
@@ -9,8 +9,8 @@ new Vue({
 	var url = "https://yuntech-braslab.github.io/xmlhttp/annolist.txt";
 	xmlhttp.onreadystatechange = function() {
 	    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-		self.annolist = xmlhttp.responseText;
-		console.log(JSON.parse(self.annolist));
+		self.annolist = JSON.parse(xmlhttp.responseText);
+		console.log(JSON.parse(xmlhttp.responseText));
 		}
 	};
 	xmlhttp.open("GET", url, true);
